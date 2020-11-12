014a8f88eb7c88c05e771f0bab827c23bdb7bf82
JimcostDev <rjak727@gmail.com>
2020-11-12 12:24:11 -0500

    Create DbContext and Model to ToDoItem

---
+++ ToDo.App/ToDo.App/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.App.Data
+{
+    public class DatabaseContext
+    {
+        private readonly SQLiteAsyncConnection Connection;
+
+        public DatabaseContext(string dbPath)
+        {
+            Connection = new SQLiteAsyncConnection(dbPath);
+
+            Connection.CreateTableAsync<ToDoItem>().Wait();
+        }
+    }
+}
---
+++ ToDo.App/ToDo.App/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.App.Models
+{
+    public class ToDoItem
+    {
+    }
+}
