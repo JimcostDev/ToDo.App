ec1e5fbac8f7eb15e523050a97ea25da00bf4ca4
JimcostDev <rjak727@gmail.com>
2020-11-12 12:36:21 -0500

    Initialize Database - localdb

--- ToDo.App/ToDo.App/App.xaml.cs
+++ ToDo.App/ToDo.App/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ToDo.App.Data;
 using ToDo.App.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -7,11 +8,18 @@ namespace ToDo.App
 {
     public partial class App : Application
     {
+        //initialize DatabaseContext
+        public static DatabaseContext Context { get; set; }
         public App()
         {
             InitializeComponent();
-
+            InitializeDatabase();
             MainPage = new NavigationPage(new HomePage());
+        }
+
+        private void InitializeDatabase()
+        {
+            var folderApp = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         }
 
         protected override void OnStart()
